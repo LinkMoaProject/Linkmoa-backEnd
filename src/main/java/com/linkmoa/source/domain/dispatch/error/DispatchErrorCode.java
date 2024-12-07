cf4181b76b43da52085f82576539c45ba66f6ac3
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2024-12-07 18:55:47 +0900

    LP-105 feat : SHARE_PAGE_INVITATION_REQUEST_NOT_FOUND 에러코드 추가

--- src/main/java/com/linkmoa/source/domain/dispatch/error/DispatchErrorCode.java
+++ src/main/java/com/linkmoa/source/domain/dispatch/error/DispatchErrorCode.java
@@ -8,7 +8,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum DispatchErrorCode implements ErrorCode {
 
-    REQUEST_ALREADY_PROCESSED(HttpStatus.NOT_FOUND , "이미 처리된 요청입니다.");
+    REQUEST_ALREADY_PROCESSED(HttpStatus.BAD_REQUEST , "이미 처리된 요청입니다."),
+    INVALID_NOTIFICATION_TYPE(HttpStatus.BAD_REQUEST, "잘못된 알림 유형입니다."),
+    INVALID_RECEIVER(HttpStatus.BAD_REQUEST, "요청 처리 권한이 없습니다. 요청 수신자만 요청을 처리할 수 있습니다"),
+    SHARE_PAGE_INVITATION_REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "공유 페이지 초대 요청을 찾을 수 없습니다.");
+
 
     private HttpStatus httpStatus;
     private String errorMessage;
