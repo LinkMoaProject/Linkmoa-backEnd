28a1105765a801d536c1a12a462a1c9368558b99
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2024-12-08 13:38:05 +0900

    LP-105 feat : 전략 패턴 설계를 위해 공통 인터페이스 DispatchProcessor.java를 추가하고, 관련 메서드들을 기본 구현으로 제공함

--- src/main/java/com/linkmoa/source/domain/dispatch/service/processor/DispatchProcessor.java
+++ src/main/java/com/linkmoa/source/domain/dispatch/service/processor/DispatchProcessor.java
@@ -2,16 +2,30 @@ package com.linkmoa.source.domain.dispatch.service.processor;
 
 import com.linkmoa.source.auth.oauth2.principal.PrincipalDetails;
 import com.linkmoa.source.domain.dispatch.constant.RequestStatus;
+import com.linkmoa.source.domain.dispatch.dto.request.DispatchProcessingRequest;
 import com.linkmoa.source.domain.dispatch.dto.response.ApiDispatchResponseSpec;
 import com.linkmoa.source.domain.dispatch.error.DispatchErrorCode;
+import com.linkmoa.source.domain.dispatch.exception.DispatchException;
+import com.linkmoa.source.domain.notify.constant.NotificationType;
 
 public interface DispatchProcessor {
-    ApiDispatchResponseSpec<?> processRequest(Long requestId, RequestStatus status, PrincipalDetails principalDetails);
+    ApiDispatchResponseSpec<?> processRequest(DispatchProcessingRequest dispatchProcessingRequest,PrincipalDetails principalDetails);
 
-    // 기본 메서드로 상태 확인 로직 추가
     default void validateRequestStatus(RequestStatus status) {
         if (status == RequestStatus.ACCEPTED || status == RequestStatus.REJECTED) {
-            throw new DispatchErrorCode(DispatchErrorCode.REQUESR_ALREADY_PROCESSED);
+            throw new DispatchException(DispatchErrorCode.REQUEST_ALREADY_PROCESSED);
+        }
+    }
+
+    default void validateNotificationType(NotificationType expectedType, NotificationType actualType) {
+        if (expectedType != actualType) {
+            throw new DispatchException(DispatchErrorCode.INVALID_NOTIFICATION_TYPE);
+        }
+    }
+
+    default void validateReceiver(String expectedReceiver, String currentMemberEmail) {
+        if (!expectedReceiver.equals(currentMemberEmail)) {
+            throw new DispatchException(DispatchErrorCode.INVALID_RECEIVER);
         }
     }
 
