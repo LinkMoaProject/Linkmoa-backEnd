3e8bceb70336e4424ccf3012a7d193c9ce493e6f
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2024-12-21 18:18:07 +0900

    LP-108 feat : PageRepositoryCustom 클래스 내 findAllPagesByMemberId 메소드 추가

--- src/main/java/com/linkmoa/source/domain/page/repository/PageRepositoryCustom.java
+++ src/main/java/com/linkmoa/source/domain/page/repository/PageRepositoryCustom.java
@@ -1,7 +1,12 @@
 package com.linkmoa.source.domain.page.repository;
 
+import com.linkmoa.source.domain.page.dto.response.ApiPageResponseSpec;
+import com.linkmoa.source.domain.page.dto.response.PagesResponse;
+
+import java.util.List;
+
 public interface PageRepositoryCustom {
 
-
+    List<ApiPageResponseSpec<PagesResponse>> findAllPagesByMemberId(Long memberId);
 
 }
