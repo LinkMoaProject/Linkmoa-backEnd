7f4c93f5054c0a85938465814cb91cd07208d498
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2024-12-23 17:00:11 +0900

    LP-112 feat : PageMainResponse 클래스 추가

--- src/main/java/com/linkmoa/source/domain/page/dto/response/PageMainResponse.java
+++ src/main/java/com/linkmoa/source/domain/page/dto/response/PageMainResponse.java
@@ -1,2 +1,20 @@
-package com.linkmoa.source.domain.page.dto.response;public class PageMainResponse {
+package com.linkmoa.source.domain.page.dto.response;
+
+import com.linkmoa.source.domain.directory.dto.response.DirectoryMainResponse;
+import com.linkmoa.source.domain.site.dto.response.SiteResponse;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record PageMainResponse(
+
+    Long pageId,
+    String pageTitle,
+    String pageDescription,
+    List<DirectoryMainResponse> directoryMainResponses,
+    List<SiteResponse> siteResponses
+
+    ){
+
 }
