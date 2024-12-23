21e997be057400766fd273b8e746d8640a23f5d7
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2024-12-23 14:23:06 +0900

    LP-108 feat : PageRepository 인터페이스에 PageRepositoryCustom 인터페이스  상속

--- src/main/java/com/linkmoa/source/domain/page/repository/PageRepository.java
+++ src/main/java/com/linkmoa/source/domain/page/repository/PageRepository.java
@@ -3,5 +3,5 @@ package com.linkmoa.source.domain.page.repository;
 import com.linkmoa.source.domain.page.entity.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PageRepository extends JpaRepository<Page,Long> {
+public interface PageRepository extends JpaRepository<Page,Long>,PageRepositoryCustom {
 }
