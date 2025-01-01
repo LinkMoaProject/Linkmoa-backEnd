8228548aba0857c9fd7931c3cbac51f4b1bd6891
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-01-01 14:52:27 +0900

    LP-112 feat: PageAsyncService 추가

--- src/main/java/com/linkmoa/source/domain/page/service/PageAsyncService.java
+++ src/main/java/com/linkmoa/source/domain/page/service/PageAsyncService.java
@@ -1,2 +1,54 @@
-package com.linkmoa.source.domain.page.service;public class PageAsyncService {
+package com.linkmoa.source.domain.page.service;
+
+
+import com.linkmoa.source.domain.directory.dto.response.DirectoryDetailResponse;
+import com.linkmoa.source.domain.directory.dto.response.DirectoryResponse;
+import com.linkmoa.source.domain.directory.repository.DirectoryRepository;
+import com.linkmoa.source.domain.page.dto.response.PageDetailsResponse;
+import com.linkmoa.source.domain.page.entity.Page;
+import com.linkmoa.source.domain.site.dto.response.SiteDetailResponse;
+import com.linkmoa.source.domain.site.repository.SiteRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@RequiredArgsConstructor
+public class PageAsyncService {
+
+    private final DirectoryRepository directoryRepository;
+    private final SiteRepository siteRepository;
+
+    @Async("threadPoolTaskExecutor")
+    public CompletableFuture<List<DirectoryDetailResponse>> findDirectoryDetailsAsync(Long directoryId) {
+        return CompletableFuture.completedFuture(directoryRepository.findDirectoryDetails(directoryId));
+    }
+
+    @Async("threadPoolTaskExecutor")
+    public CompletableFuture<List<SiteDetailResponse>> findSitesDetailsAsync(Long directoryId){
+        return CompletableFuture.completedFuture(siteRepository.findSitesDetails(directoryId));
+    }
+
+    public CompletableFuture<PageDetailsResponse> combinePageDetails(
+            CompletableFuture<List<DirectoryDetailResponse>> directoryDetailsFuture,
+            CompletableFuture<List<SiteDetailResponse>> sitesDetailsFuture,
+            Page page
+    ) {
+        return directoryDetailsFuture.thenCombine(sitesDetailsFuture, (directoryDetails, sitesDetails) -> {
+            // 두 비동기 결과를 결합하여 페이지 세부 정보를 준비
+            PageDetailsResponse pageDetailsResponse = PageDetailsResponse.builder()
+                    .pageId(page.getId())
+                    .pageTitle(page.getPageTitle())
+                    .pageDescription(page.getPageDescription())
+                    .directoryDetailRespons(directoryDetails)
+                    .siteDetailResponses(sitesDetails)
+                    .build();
+            return pageDetailsResponse;
+        });
+    }
+
 }
