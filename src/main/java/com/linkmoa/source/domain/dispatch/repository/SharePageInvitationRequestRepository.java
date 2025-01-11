5dec34b0e777eb30774d069f0632b6f2f8ae2283
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-01-11 15:37:10 +0900

    LP-119 feat : SharePageInvitationRequestRepository 인터페이스에 SharePageInvitationRequestRepositoryCustom 인터페이스 확장

--- src/main/java/com/linkmoa/source/domain/dispatch/repository/SharePageInvitationRequestRepository.java
+++ src/main/java/com/linkmoa/source/domain/dispatch/repository/SharePageInvitationRequestRepository.java
@@ -3,5 +3,5 @@ package com.linkmoa.source.domain.dispatch.repository;
 import com.linkmoa.source.domain.dispatch.entity.SharePageInvitationRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SharePageInvitationRequestRepository extends JpaRepository<SharePageInvitationRequest,Long> {
+public interface SharePageInvitationRequestRepository extends JpaRepository<SharePageInvitationRequest,Long>,SharePageInvitationRequestRepositoryCustom {
 }
