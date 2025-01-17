eedea2972f6711b082b1706e42ac0c98a56fa1bc
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-01-17 19:11:06 +0900

    LP-123 feat : 알람 목록 조회 시 , 미확인 알람 개수를 사용자에게 실시간으로 응답

--- src/main/java/com/linkmoa/source/domain/dispatch/service/DispatchRequestService.java
+++ src/main/java/com/linkmoa/source/domain/dispatch/service/DispatchRequestService.java
@@ -19,6 +19,7 @@ import com.linkmoa.source.domain.member.exception.MemberException;
 import com.linkmoa.source.domain.member.service.MemberService;
 import com.linkmoa.source.domain.notification.aop.annotation.NotificationApplied;
 import com.linkmoa.source.domain.notification.repository.NotificationRepository;
+import com.linkmoa.source.domain.notification.service.NotificationService;
 import com.linkmoa.source.domain.page.contant.PageType;
 import com.linkmoa.source.domain.page.dto.response.ApiPageResponseSpec;
 import com.linkmoa.source.domain.page.entity.Page;
@@ -44,6 +45,8 @@ public class DispatchRequestService {
     private final PageRepository pageRepository;
     private final SharePageInvitationRequestRepository sharePageInvitationRequestRepository;
     private final NotificationRepository notificationRepository;
+    private final NotificationService notificationService;
+
 
     @Transactional
     @ValidationApplied
@@ -136,7 +139,7 @@ public class DispatchRequestService {
         return allDirectoryTransmissionRequestByReceiverEmail;
     }
 
-    @Transactional // 이 메서드가 트랜잭션 내에서 실행되도록 보장
+    @Transactional
     public ApiDispatchResponseSpec<NotificationsDetailsResponse> findAllNotificationsForReceiver(String receiverEmail){
         NotificationsDetailsResponse notificationDetails = NotificationsDetailsResponse.builder()
                 .DirectoryTransmissionRequests(findDirectoryDirectoryTransmissionsForReceiver(receiverEmail))
@@ -144,6 +147,7 @@ public class DispatchRequestService {
                 .build();
 
         notificationRepository.updateUnreadNotificationsToReadByReceiverEmail(receiverEmail);
+        notificationService.sendUnreadNotificationCount(receiverEmail);
 
         return ApiDispatchResponseSpec.<NotificationsDetailsResponse>builder()
                 .httpStatusCode(HttpStatus.OK)
