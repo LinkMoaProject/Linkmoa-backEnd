f52049505728db5677eaf6b523822b083d10b947
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-01-17 19:59:18 +0900

    WIP: DispatchRequestService 변경 사항 저장

--- src/main/java/com/linkmoa/source/domain/dispatch/service/DispatchRequestService.java
+++ src/main/java/com/linkmoa/source/domain/dispatch/service/DispatchRequestService.java
@@ -147,7 +147,9 @@ public class DispatchRequestService {
                 .build();
 
         notificationRepository.updateUnreadNotificationsToReadByReceiverEmail(receiverEmail);
+
         notificationService.sendUnreadNotificationCount(receiverEmail);
+
         return ApiDispatchResponseSpec.<NotificationsDetailsResponse>builder()
                 .httpStatusCode(HttpStatus.OK)
                 .successMessage("알람 목록을 조회했습니다.")
