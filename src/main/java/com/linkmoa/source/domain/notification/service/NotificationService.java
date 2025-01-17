14ccacd401cf981e0428339498bbd274f51353d0
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-01-17 20:05:35 +0900

    충돌 병합

--- src/main/java/com/linkmoa/source/domain/notification/service/NotificationService.java
+++ src/main/java/com/linkmoa/source/domain/notification/service/NotificationService.java
@@ -142,5 +142,10 @@ public class NotificationService {
         notificationRepository.deleteAllBySenderEmailOrReceiverEmail(email);
     }
 
+    public void temp() {
+        System.out.println("test");
+    }
+
+
 
 }
