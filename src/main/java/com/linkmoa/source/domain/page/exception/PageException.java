f0ec2ecf98039569c3b2579e1023edce8727d76d
Gyuri <haha0888@naver.com>
2025-01-23 09:51:57 +0900

    refactor : PageException 생성자 추가 - 에러 메세지 못 불러옴.

--- src/main/java/com/linkmoa/source/domain/page/exception/PageException.java
+++ src/main/java/com/linkmoa/source/domain/page/exception/PageException.java
@@ -1,12 +1,22 @@
 package com.linkmoa.source.domain.page.exception;
 
 
+import com.linkmoa.source.domain.member.error.MemberErrorCode;
 import com.linkmoa.source.domain.page.error.PageErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
-@RequiredArgsConstructor
 public class PageException extends RuntimeException {
     private final PageErrorCode pageErrorCode;
+
+    public PageException(PageErrorCode pageErrorCode){
+        super(pageErrorCode.getErrorMessage());
+        this.pageErrorCode = pageErrorCode;
+    }
+    public HttpStatus getHttpStatus() {
+        return pageErrorCode.getHttpStatus();
+    }
+
 }
