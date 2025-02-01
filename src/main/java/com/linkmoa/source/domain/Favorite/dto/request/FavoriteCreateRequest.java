201dab5285cecf8f78ffc4f03e2a35c6bb2caf21
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-02-01 14:27:33 +0900

    LP-149 refactor : FavoriteCreateRequest.java 필드 수정

--- src/main/java/com/linkmoa/source/domain/Favorite/dto/request/FavoriteCreateRequest.java
+++ src/main/java/com/linkmoa/source/domain/Favorite/dto/request/FavoriteCreateRequest.java
@@ -1,10 +1,11 @@
 package com.linkmoa.source.domain.Favorite.dto.request;
 
 import com.linkmoa.source.domain.Favorite.constant.FavoriteType;
+import jakarta.validation.constraints.NotNull;
 
 public record FavoriteCreateRequest(
-        Long itemId,
-        FavoriteType favoriteType
+        @NotNull Long itemId,
+        @NotNull FavoriteType favoriteType
 
 ) {
 }
