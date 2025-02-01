ac50e839487dc9ffa11b19493212a0731f7bb098
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-02-01 13:46:52 +0900

    LP-149 feat: FavoriteCreateResponse 추가

---
+++ src/main/java/com/linkmoa/source/domain/Favorite/dto/response/FavoriteCreateResponse.java
@@ -0,0 +1,13 @@
+package com.linkmoa.source.domain.Favorite.dto.response;
+
+import com.linkmoa.source.domain.Favorite.constant.FavoriteType;
+import lombok.Builder;
+
+@Builder
+public record FavoriteCreateResponse(
+        Long itemId,
+        FavoriteType favoriteType,
+        String itemName
+
+) {
+}
