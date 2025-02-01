01123bbb69faabdfa0eafc1064cfccebe6c56170
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-02-01 14:06:27 +0900

    LP-149 refactor : ApiFavoriteResponseSpec 파라미터 수정

--- src/main/java/com/linkmoa/source/domain/Favorite/dto/response/ApiFavoriteResponseSpec.java
+++ src/main/java/com/linkmoa/source/domain/Favorite/dto/response/ApiFavoriteResponseSpec.java
@@ -2,6 +2,7 @@ package com.linkmoa.source.domain.Favorite.dto.response;
 
 
 import com.linkmoa.source.global.spec.ApiResponseSpec;
+import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
@@ -9,7 +10,10 @@ import org.springframework.http.HttpStatus;
 public class ApiFavoriteResponseSpec<T> extends ApiResponseSpec {
 
     private T data;
-    public ApiFavoriteResponseSpec(HttpStatus httpStatusCode, String successMessage) {
+
+    @Builder
+    public ApiFavoriteResponseSpec(HttpStatus httpStatusCode, String successMessage,T data) {
         super(httpStatusCode, successMessage);
+        this.data=data;
     }
 }
