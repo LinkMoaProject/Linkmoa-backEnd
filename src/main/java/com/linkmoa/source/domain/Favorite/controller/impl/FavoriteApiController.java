0a34ca1a0e0f3325f6e8dd2edac3c89a8d8f701f
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-02-01 14:27:00 +0900

    LP-149 feat : 즐겨찾기 컨트롤러 추기

---
+++ src/main/java/com/linkmoa/source/domain/Favorite/controller/impl/FavoriteApiController.java
@@ -0,0 +1,31 @@
+package com.linkmoa.source.domain.Favorite.controller.impl;
+
+import com.linkmoa.source.auth.oauth2.principal.PrincipalDetails;
+import com.linkmoa.source.domain.Favorite.controller.spec.FavoriteApiSpecification;
+import com.linkmoa.source.domain.Favorite.dto.request.FavoriteCreateRequest;
+import com.linkmoa.source.domain.Favorite.dto.response.ApiFavoriteResponseSpec;
+import com.linkmoa.source.domain.Favorite.dto.response.FavoriteCreateResponse;
+import com.linkmoa.source.domain.Favorite.service.FavoriteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/favorite")
+public class FavoriteApiController implements FavoriteApiSpecification {
+
+    private final FavoriteService favoriteService;
+
+    @Override
+    public ResponseEntity<ApiFavoriteResponseSpec<FavoriteCreateResponse>> createFavorite(
+            FavoriteCreateRequest favoriteCreateRequest,
+            PrincipalDetails principalDetails) {
+
+        ApiFavoriteResponseSpec<FavoriteCreateResponse> favoriteCreateResponse = favoriteService.createFavorite(favoriteCreateRequest, principalDetails);
+
+        return ResponseEntity.ok().body(favoriteCreateResponse);
+    }
+}
---
+++ src/main/java/com/linkmoa/source/domain/Favorite/controller/spec/FavoriteApiSpecification.java
@@ -0,0 +1,30 @@
+package com.linkmoa.source.domain.Favorite.controller.spec;
+
+import com.linkmoa.source.auth.oauth2.principal.PrincipalDetails;
+import com.linkmoa.source.domain.Favorite.dto.request.FavoriteCreateRequest;
+import com.linkmoa.source.domain.Favorite.dto.response.ApiFavoriteResponseSpec;
+import com.linkmoa.source.domain.Favorite.dto.response.FavoriteCreateResponse;
+import com.linkmoa.source.domain.Favorite.error.FavoriteErrorCode;
+import com.linkmoa.source.global.swagger.ApiErrorCodeExamples;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface FavoriteApiSpecification {
+
+
+    @Tag(name = "Favorite", description = "즐겨찾기 관련 API")
+    @Operation(summary = "즐겨찾기 생성", description = "아이템(디레토리,사이트)를 즐겨찾기에 등록합니다.")
+    @ApiErrorCodeExamples(FavoriteErrorCode.class)
+    @PostMapping
+    @PreAuthorize("isAuthenticated()")
+    ResponseEntity<ApiFavoriteResponseSpec<FavoriteCreateResponse>> createFavorite(
+            @RequestBody @Validated FavoriteCreateRequest favoriteCreateRequest,
+            @AuthenticationPrincipal PrincipalDetails principalDetails
+    );
+}
