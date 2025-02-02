ea995603b2794bd4dcd4cf24d30f76df20da2045
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-02-02 19:16:54 +0900

    LP-155 feat : decrementFavoriteOrderIndexes 메소드 추가

--- src/main/java/com/linkmoa/source/domain/Favorite/repository/FavoriteRepositoryCustom.java
+++ src/main/java/com/linkmoa/source/domain/Favorite/repository/FavoriteRepositoryCustom.java
@@ -3,6 +3,7 @@ package com.linkmoa.source.domain.Favorite.repository;
 import com.linkmoa.source.domain.member.entity.Member;
 
 public interface FavoriteRepositoryCustom {
-
     public void incrementOrderIndexesForMember(Member member);
+    public void decrementFavoriteOrderIndexes(Integer orderIndex);
+
 }
