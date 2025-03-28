4aa895aac359b1e18b0aafda4fd265c67d3b9dcf
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-03-28 19:25:05 +0900

    LP-171 refactor : DirectoryTransmissionRequestCreate 필수 파라미터 유효성 어노테이션 추가

--- src/main/java/com/linkmoa/source/domain/dispatch/dto/request/DirectoryTransmissionRequestCreate.java
+++ src/main/java/com/linkmoa/source/domain/dispatch/dto/request/DirectoryTransmissionRequestCreate.java
@@ -2,12 +2,13 @@ package com.linkmoa.source.domain.dispatch.dto.request;
 
 import com.linkmoa.source.global.dto.request.BaseRequest;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record DirectoryTransmissionRequestCreate(
 	BaseRequest baseRequest,
-	String receiverEmail,
-	Long directoryId
+	@NotNull String receiverEmail,
+	@NotNull Long directoryId
 ) {
 }
