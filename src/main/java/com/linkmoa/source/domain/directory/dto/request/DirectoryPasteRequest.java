0bbcf5c893c8d112eded6f0ab53777deff825081
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-03-28 19:18:01 +0900

    LP-171 refactor : DirectoryPasteRequest 필수 파라미터 유효성 어노테이션(@NotNull) 추가

--- src/main/java/com/linkmoa/source/domain/directory/dto/request/DirectoryPasteRequest.java
+++ src/main/java/com/linkmoa/source/domain/directory/dto/request/DirectoryPasteRequest.java
@@ -2,10 +2,12 @@ package com.linkmoa.source.domain.directory.dto.request;
 
 import com.linkmoa.source.global.dto.request.BaseRequest;
 
+import jakarta.validation.constraints.NotNull;
+
 public record DirectoryPasteRequest(
 
 	BaseRequest baseRequest,
-	Long originalDirectoryId,
-	Long destinationDirectoryId
+	@NotNull Long originalDirectoryId,
+	@NotNull Long destinationDirectoryId
 ) {
 }
