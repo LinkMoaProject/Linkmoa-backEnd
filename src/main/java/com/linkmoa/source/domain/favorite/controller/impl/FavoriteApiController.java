3c13545a2233417cb1e2792cdd893a8f4e364e8e
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-03-28 19:29:14 +0900

    LP-171 refactor: FavoriteApiController 구조 개선

--- src/main/java/com/linkmoa/source/domain/favorite/controller/impl/FavoriteApiController.java
+++ src/main/java/com/linkmoa/source/domain/favorite/controller/impl/FavoriteApiController.java
@@ -1,11 +1,16 @@
 package com.linkmoa.source.domain.favorite.controller.impl;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.linkmoa.source.auth.oauth2.principal.PrincipalDetails;
-import com.linkmoa.source.domain.favorite.controller.spec.FavoriteApiSpecification;
 import com.linkmoa.source.domain.favorite.dto.request.FavoriteUpdateRequest;
 import com.linkmoa.source.domain.favorite.dto.response.ApiFavoriteResponseSpec;
 import com.linkmoa.source.domain.favorite.dto.response.FavoriteDetailResponse;
@@ -17,13 +22,15 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/favorite")
-public class FavoriteApiController implements FavoriteApiSpecification {
+public class FavoriteApiController {
 
 	private final FavoriteService favoriteService;
 
+	@PostMapping
+	@PreAuthorize("isAuthenticated()")
 	public ResponseEntity<ApiFavoriteResponseSpec<FavoriteResponse>> updateFavorite(
-		FavoriteUpdateRequest favoriteUpdateRequest,
-		PrincipalDetails principalDetails) {
+		@RequestBody @Validated FavoriteUpdateRequest favoriteUpdateRequest,
+		@AuthenticationPrincipal PrincipalDetails principalDetails) {
 
 		ApiFavoriteResponseSpec<FavoriteResponse> favoriteCreateResponse = favoriteService.updateFavorite(
 			favoriteUpdateRequest, principalDetails);
@@ -31,8 +38,10 @@ public class FavoriteApiController implements FavoriteApiSpecification {
 		return ResponseEntity.ok().body(favoriteCreateResponse);
 	}
 
+	@GetMapping
+	@PreAuthorize("isAuthenticated()")
 	public ResponseEntity<ApiFavoriteResponseSpec<FavoriteDetailResponse>> getFavorite(
-		PrincipalDetails principalDetails) {
+		@AuthenticationPrincipal PrincipalDetails principalDetails) {
 
 		ApiFavoriteResponseSpec<FavoriteDetailResponse> favoriteDetails = favoriteService.findFavoriteDetails(
 			principalDetails);
