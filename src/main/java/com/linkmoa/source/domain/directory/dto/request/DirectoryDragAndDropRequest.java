7270228376c0e0effe77e1f2c4906bbe1b052f50
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-03-28 19:19:20 +0900

    LP-171 refactor : DirectoryDragAndDropRequest 필수 파라미터 유효성 어노테이션 추가

--- src/main/java/com/linkmoa/source/domain/directory/dto/request/DirectoryDragAndDropRequest.java
+++ src/main/java/com/linkmoa/source/domain/directory/dto/request/DirectoryDragAndDropRequest.java
@@ -2,15 +2,16 @@ package com.linkmoa.source.domain.directory.dto.request;
 
 import com.linkmoa.source.global.dto.request.BaseRequest;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record DirectoryDragAndDropRequest(
 	BaseRequest baseRequest,
-	Long targetId,
-	TargetType targetType,
-	Integer targetOrderIndex,
-	Long parentDirectoryId
+	@NotNull Long targetId,
+	@NotNull TargetType targetType,
+	@NotNull Integer targetOrderIndex,
+	@NotNull Long parentDirectoryId
 ) {
 
 	public enum TargetType {
