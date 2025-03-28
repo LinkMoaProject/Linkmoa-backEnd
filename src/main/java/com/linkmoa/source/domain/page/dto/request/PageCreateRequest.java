18660fb4692b1bddefb28c27192fc3361a7eacc6
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-03-28 21:13:13 +0900

    LP-171 refactor : PageCreateRequest dto 필드 수정

--- src/main/java/com/linkmoa/source/domain/page/dto/request/PageCreateRequest.java
+++ src/main/java/com/linkmoa/source/domain/page/dto/request/PageCreateRequest.java
@@ -1,7 +1,6 @@
 package com.linkmoa.source.domain.page.dto.request;
 
 import com.linkmoa.source.domain.page.contant.PageType;
-import com.linkmoa.source.global.constant.CommandType;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -11,7 +10,6 @@ import lombok.Builder;
 public record PageCreateRequest(
 	@Size(max = 50) String pageTitle,
 	@Size(max = 100) String pageDescription,
-	@NotNull PageType pageType,
-	CommandType commandType
+	@NotNull PageType pageType
 ) {
 }
