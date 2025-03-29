2e5b07efa84b8281221c98c11c5c4add329f2638
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2025-03-29 17:46:14 +0900

    LP-171 refactor : findFavoriteSites 메서드 반환 타입 수정

--- src/main/java/com/linkmoa/source/domain/site/repository/SiteRepositoryCustom.java
+++ src/main/java/com/linkmoa/source/domain/site/repository/SiteRepositoryCustom.java
@@ -3,10 +3,11 @@ package com.linkmoa.source.domain.site.repository;
 import java.util.List;
 
 import com.linkmoa.source.domain.site.dto.response.SiteDetailResponse;
+import com.linkmoa.source.domain.site.dto.response.SiteSimpleResponse;
 
 public interface SiteRepositoryCustom {
 	List<SiteDetailResponse> findSitesDetails(Long directoryId, List<Long> favoriteSiteIds);
 
-	List<SiteDetailResponse> findFavoriteSites(List<Long> favoriteSitesIds);
+	List<SiteSimpleResponse> findFavoriteSites(List<Long> favoriteSitesIds);
 
 }
